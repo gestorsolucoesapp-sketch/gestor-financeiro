7a00dc0d56a72c0945abad2e3ccf2e50e3ae019b
gestorsolucoesapp-sketch <gestorsolucoesapp@gmail.com>
2026-07-13 07:01:37 -0300

    SW v100

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Gestor Soluções · Service Worker (offline shell + push)
-const CACHE = 'gf-shell-v99';
+const CACHE = 'gf-shell-v100';
 const CORE = [
   './', 'index.html', 'manifest.json',
   'icon-192.png', 'icon-512.png', 'apple-touch-icon.png',
